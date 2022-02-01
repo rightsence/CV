bbb89d1429859191aababbdce0163ed3616441e6
rightsence <96792956+rightsence@users.noreply.github.com>
2022-02-01 14:20:46 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+VANTA.WAVES({
+    el: "#intro",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x0,
+    shininess: 69.00,
+    waveHeight: 11.00,
+    waveSpeed: 0.45,
+    zoom: 0.75
+  })
